2ba35a17f2de8df51a9df646280ed24d20810a39
Nagaraja Settra <nagams@gmail.com>
2025-03-07 11:38:39 -0500

    initial setup for the main content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '', 
+        component: HomeComponent,
+        title: 'Home page',
+    },
+    { 
+        path: 'about', 
+        component: AboutComponent,
+        title: 'About',
+    },
+    { 
+        path: 'contact', 
+        component: ContactComponent,
+        title: 'Contact',
+    },
+    { 
+        path: 'gallery', 
+        component: GalleryComponent,
+        title: 'Gallery',
+    },
+    { 
+        path: 'menu', 
+        component: MenuComponent,
+        title: 'Menu',
+    },
+];
